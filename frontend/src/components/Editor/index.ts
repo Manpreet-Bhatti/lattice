6e3bcae215e016bc4c2ce31be104bbb98c67883d
Manpreet Bhatti <manpreet@bhatti.net>
2025-11-30 17:32:30 -0500

    Update

--- frontend/src/components/Editor/index.ts
+++ frontend/src/components/Editor/index.ts
@@ -1,1 +1,8 @@
+export { CodeMirrorEditor } from "./CodeMirrorEditor";
 export { Editor } from "./Editor";
+export {
+  languages,
+  getLanguageFromFilename,
+  getLanguageSupport,
+} from "./languages";
+export type { LanguageId } from "./languages";
